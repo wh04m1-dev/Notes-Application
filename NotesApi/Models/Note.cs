5c54f93035a8d0f904f4fae3d4227428ca9c2fca
Sothea <officalsengchanthea@gmail.com>
2025-06-18 07:36:36 +0700

    push

--- NotesApi/Models/Note.cs
+++
@@ -1,9 +0,0 @@
-public class Note
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string Content { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
-    public int UserId { get; set; }
-}
--- NotesApi/Models/User.cs
+++
@@ -1,8 +0,0 @@
-public class User
-{
-    public int Id { get; set; }
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public byte[] PasswordHash { get; set; }
-    public byte[] PasswordSalt { get; set; }
-}
